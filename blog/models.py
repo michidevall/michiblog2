d855b6ae647f7e7f6923760eae61aaeb4564effe
Michi Devall <85964463+michidevall@users.noreply.github.com>
2023-08-17 17:07:34 -0600

    blog_post correction

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="blog_post"
     )
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
